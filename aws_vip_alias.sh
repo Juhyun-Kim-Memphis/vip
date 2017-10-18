e02c56f07f0a7b1525c784cafed4d0ccd6a1673c
Juhyun-Kim-Memphis <kjh.memphis@gmail.com>
2017-10-18 16:24:43 +0900

    vip first build (before test)

--- aws_vip_alias.sh
+++ aws_vip_alias.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 my_private_ipv4=`ec2-metadata -o|awk '{print $2;}'`
-
+#my_private_ipv4='127.0.0.1'
 echo "my ip is $my_private_ipv4 "
 
-echo "'NetworkInterfaces[?PrivateIpAddress==`$my_private_ipv4`].NetworkInterfaceId'"
+echo "aws ec2 describe-network-interfaces --query 'NetworkInterfaces[?PrivateIpAddress==\`$my_private_ipv4\`].NetworkInterfaceId'"
+
+aws ec2 describe-network-interfaces --query 'NetworkInterfaces[?PrivateIpAddress==\`$my_private_ipv4\`].NetworkInterfaceId'
